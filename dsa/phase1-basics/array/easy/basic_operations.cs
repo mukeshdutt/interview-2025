0cf7ebc6c102bb425ea55b9c75e2c0089bfd5f89
mukeshdutt <mukeshdutt@outlook.com>
2024-11-16 08:28:33 +0530

    basic operations

---
+++ dsa/phase1-basics/array/easy/basic_operations.cs
@@ -0,0 +1,22 @@
+class ArrayBasicOperations
+{
+    int[] Insertion(int[] items, int position)
+    {
+        return [];
+    }
+
+    int[] Deletion(int[] items, int position)
+    {
+        return [];
+    }
+
+    int[] Replace(int[] items, int position, int item)
+    {
+        return [];
+    }
+
+    int Searching(int[] items, int item)
+    {
+        return 1;
+    }
+}

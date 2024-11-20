ac7c3a7b0eef927b4f07cfab68ae193ebb069f83
mukeshdutt <mukeshdutt@outlook.com>
2024-11-20 09:51:05 +0530

    simple search

--- dsa/phase1-basics/array/easy/basic_operations.cs
+++ dsa/phase1-basics/array/easy/basic_operations.cs
@@ -1,22 +1,48 @@
+
 class ArrayBasicOperations
 {
-    int[] Insertion(int[] items, int position)
+    void Insertion(int[] items, int position, int value)
     {
-        return [];
+        if (position < 0)
+        {
+
+        }
+
+        int[] arrResized = new int[items.Length];
+        for (int i = 0; i < items.Length; i++)
+        {
+
+        }
     }
 
-    int[] Deletion(int[] items, int position)
+    void Deletion(int[] items, int position)
     {
-        return [];
+
     }
 
-    int[] Replace(int[] items, int position, int item)
+    void Replace(int[] items, int position, int item)
     {
-        return [];
+
     }
 
-    int Searching(int[] items, int item)
+    public static void Searching(int[] items, int item)
     {
-        return 1;
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == item)
+            {
+                System.Console.WriteLine($"Item found at position: {i}");
+                return;
+            }
+        }
+        System.Console.WriteLine("Record not found.");
+    }
+
+    public static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            System.Console.WriteLine(arr[i]);
+        }
     }
 }
